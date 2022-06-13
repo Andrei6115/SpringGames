877ed1f430a436d76c59c8bff6280fd8cb09a31b
Cred Alecia <calecia@pl-h1.msk.21-school.ru>
2022-06-13 21:25:42 +0300

    properties config

--- SpringDestroyer/src/main/java/quoters/TerminatorQuoter.java
+++ SpringDestroyer/src/main/java/quoters/TerminatorQuoter.java
@@ -15,6 +15,10 @@ public class TerminatorQuoter implements Quoter{
 	@InjectRandomInt(min = 2, max = 7)
 	private int repeat;
 
+	public void setRepeat(int repeat) {
+		this.repeat = repeat;
+	}
+
 	@PostConstruct
 	public void init() {
 		System.out.println(TEXT_GREEN + "Part two, repeat = " + repeat + TEXT_RESET);
